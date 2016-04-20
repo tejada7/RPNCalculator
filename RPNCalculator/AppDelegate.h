f8a13c7b617e904eb3717141ee0914010a13cc39
Favio <Favio@favios-imac.local>
2016-04-20 18:40:27 +0200

    Initial Commit

---
+++ RPNCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RPNCalculator
+//
+//  Created by Favio on 4/20/16.
+//  Copyright © 2016 Favio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RPNCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RPNCalculator
+//
+//  Created by Favio on 4/20/16.
+//  Copyright © 2016 Favio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
